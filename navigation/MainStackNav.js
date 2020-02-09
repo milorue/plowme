8ca7ae0fc6d6dd466c4b1922c30304158800442d
milorue <milorue@gmail.com>
2020-02-09 03:16:40 -0500

    02-09-2020 alpha-version 0.1

---
+++ navigation/MainStackNav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs";
+
+const LoginStack = createStackNavigator({
+    Login: {
+
+    }
+})
